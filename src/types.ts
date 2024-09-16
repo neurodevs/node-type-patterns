4388e54c35ad3418f55e7fb6c000b35389594b46
eric-yates <hello@ericthecurious.com>
2024-09-16 00:42:32 -0600

    minor: create generic types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,15 @@
-export interface FileLoader<T> {
-    load: (path: string) => Promise<T>
+export interface FileLoader<Content> {
+    load: (path: string) => Promise<Content>
+}
+
+export interface Extractor<Content, Result> {
+    extract: (data: Content) => Result
+}
+
+export interface SyncRunnable<Result = void> {
+    run: () => Result
+}
+
+export interface AsyncRunnable<Result = void> {
+    run: () => Promise<Result>
 }
